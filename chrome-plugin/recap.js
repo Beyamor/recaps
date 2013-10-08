8f875d9be8fafcedb264900ed19940d761159139
beyamor <beyamor@gmail.com>
2013-10-08 11:53:15 -0700

    Recaps widget with a close button

--- chrome-plugin/recap.js
+++ chrome-plugin/recap.js
@@ -1,1 +1,40 @@
-document.body.style.backgroundColor = "red";
+function setStyle(el, properties) {
+	
+	var style = "";
+	for (property in properties) {
+
+		style = style + property + ":" + properties[property] + ";";
+	}
+
+	el.setAttribute("style", style);
+};
+
+function createButton(label, onClick) {
+
+	var button = document.createElement("button");
+	button.setAttribute("type", "button");
+	button.textContent = label;
+	button.onclick = onClick;
+
+	return button;
+};
+
+
+if (!document.getElementById("wits-recap-widget")) {
+
+	var widget = document.createElement("div");
+	widget.setAttribute("id", "wits-recap-widget");
+	setStyle(widget, {
+		position: "fixed",
+		right: "0px",
+		top: "0px"
+	});
+
+	closeButton = createButton("cancel", function() {
+
+		document.body.removeChild(widget);
+	});
+	widget.appendChild(closeButton);
+
+	document.body.appendChild(widget);
+}
