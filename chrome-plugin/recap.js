fb992506bd0acd626a67c24aab1329a987f78d0f
beyamor <beyamor@gmail.com>
2013-10-08 12:26:03 -0700

    Capturing selector change

--- chrome-plugin/recap.js
+++ chrome-plugin/recap.js
@@ -1,13 +1,71 @@
 var CATEGORIES = [
-	"topsaucetoid",
-	"wordtoid",
-	"contestoid",
-	"communitoid",
-	"gametoid",
-	"culturoid",
-	"otheroid",
-	"failtoid"
+	"Topsaucetoid",
+	"Wordtoid",
+	"Contestoid",
+	"Communitoid",
+	"Gametoid",
+	"Culturoid",
+	"Otheroid",
+	"Failtoid"
 ];
+
+var SUBCATEGORIES = {
+	Wordtoid: [
+		["A", "Articles"],
+		["S", "Series"],
+		["M", "Monthly Musings"],
+		["P", "Podcasts"]
+	],
+
+	Contestoid: [
+		["C", "Community Contents"],
+		["W", "Winnders/Updates"],
+		["E", "Entries"]
+	],
+
+	Communitoid: [
+		["E", "Events"],
+		["F", "Fight Nights"],
+		["D", "Destructoid in the Wild"],
+		["S", "Stories"],
+		["C", "Contemplations"],
+		["I", "Introductions"],
+		["B", "Birthdays"],
+		["R", "RIP"],
+		["H", "Houses, cribs, setups"]
+	],
+
+	Gametoid: [
+		["N", "News"],
+		["V", "Videos"],
+		["R", "Reviews"],
+		["P", "Previews"],
+		["T", "Thoughts"],
+		["D", "Development"],
+		["$", "Deals"]
+	],
+
+	Culturoid: [
+		"Art",
+		"Music",
+		"Film/TV",
+		"Literature",
+		"Swag"
+	],
+
+	Otheroid: [
+		["L", "LOLs"],
+		["R", "Random"],
+		["V", "Videos"],
+		["C", "Could Be Better"],
+		["?", "Defies Description"]
+	],
+
+	Failtoid: [
+		["S", "You Are Slow"],
+		["F", "Maybe Fail?"]
+	]
+};
 
 function setStyle(el, properties) {
 	
@@ -30,16 +88,39 @@ function createButton(label, onClick) {
 	return button;
 };
 
-function createSelector(options) {
+function createSelector(options, onChange) {
 
 	var selector = document.createElement("select");
 	for (var optionIndex = 0; optionIndex < options.length; ++optionIndex) {
 
-		var option = document.createElement("option");
-		option.textContent = options[optionIndex];
-		option.setAttribute("value", options[optionIndex]);
+		var	option		= options[optionIndex],
+			optionEl	= document.createElement("option"),
+			label, value;
 
-		selector.appendChild(option);
+		if (option instanceof Array) {
+
+			label	= option[1];
+			value	= option[0];
+		}
+
+		else {
+
+			value = label = option;
+		}
+
+		optionEl.textContent = label;
+		optionEl.setAttribute("value", value);
+
+		selector.appendChild(optionEl);
+	}
+
+	if (onChange) {
+
+		selector.onchange = function() {
+
+			var value = selector.options[selector.selectedIndex].text;
+			onChange(value);
+		};
 	}
 
 	return selector;
@@ -56,8 +137,16 @@ if (!document.getElementById("wits-recap-widget")) {
 		top: "0px"
 	});
 
-	var categories = createSelector(CATEGORIES);
-	widget.appendChild(categories);
+	var categoriesContainer = document.createElement("div");
+	widget.appendChild(categoriesContainer);
+
+	var categories = createSelector(CATEGORIES, function(value) {
+		var div = document.createElement("div");
+		div.textContent = value;
+		widget.appendChild(div);
+	});
+	setStyle(categories, {display: "block"});
+	categoriesContainer.appendChild(categories);
 
 	var closeButton = createButton("cancel", function() {
 
