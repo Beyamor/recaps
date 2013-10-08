65760232e73ad741a082a0001bd214cec05a4590
beyamor <beyamor@gmail.com>
2013-10-08 13:13:22 -0700

    Started writing the server

---
+++ recaps/app.py
@@ -0,0 +1,9 @@
+from flask import Flask, Response
+app = Flask(__name__)
+
+@app.route("/recap-entry", methods=["POST"])
+def post_recap_entry():
+	return "Got it", 200
+
+if __name__ == "__main__":
+	app.run()
