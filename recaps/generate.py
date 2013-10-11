db998cabbc46324305028c115a59d2f3198f845a
beyamor <beyamor@gmail.com>
2013-10-11 11:13:30 -0700

    Isms

--- recaps/generate.py
+++ recaps/generate.py
@@ -2,9 +2,13 @@ def image(src):
 	return "[img]%s[/img]" % src
 
 def generate(data):
-	caps = {}
-	caps['header'] = image(data['recapper']['header'])
+	caps = {
+			'header': image(data['recapper']['header']),
+			'isms': data['isms']
+	}
 	return \
 """
 %(header)s
+
+%(isms)s
 """ % caps
