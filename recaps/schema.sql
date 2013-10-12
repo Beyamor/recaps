863da536954a0e2045a93361188d156ee4830408
beyamor <beyamor@gmail.com>
2013-10-12 15:13:26 -0700

    Added the other recappers

--- recaps/schema.sql
+++ recaps/schema.sql
@@ -25,5 +25,13 @@ create table recaps (
 	foreign key(recapper) references recappers(name)
 );
 
-insert into recappers ("name", "header") values
-	("Beyamor", "http://i763.photobucket.com/albums/xx276/l6t4g1/beyamorisms.jpg");
+insert into recappers (name, header) values ("Beyamor", "http://i763.photobucket.com/albums/xx276/l6t4g1/beyamorisms.jpg");
+insert into recappers (name, header) values ("bbain", "http://i.imgur.com/7JkKE.jpg");
+insert into recappers (name, header) values ("ShadeOfLight", "http://i.imgur.com/vDz3T.jpg");
+insert into recappers (name, header) values ("StriderHoang", "http://i763.photobucket.com/albums/xx276/l6t4g1/striderisms.jpg");
+insert into recappers (name, header) values ("Wrenchfarm", "http://i763.photobucket.com/albums/xx276/l6t4g1/wrenchisms.jpg");
+insert into recappers (name, header) values ("MacManus", "http://i.imgur.com/vVn2aMv.jpg?1");
+insert into recappers (name, header) values ("smurfee mcfee", "http://i224.photobucket.com/albums/dd51/smurfee_mcgee/mcgeeisms2.jpg");
+insert into recappers (name, header) values ("PhilKenSebben", "http://i.imgur.com/pLuvV.jpg");
+insert into recappers (name, header) values ("Jinx01", "http://www.destructoid.com//ul/user/2/25438-255803-cbjinxisms02jpg-620x.jpg");
+insert into recappers (name, header) values ("TheManchild", "http://i230.photobucket.com/albums/ee86/joelphosis/mangasms.jpg~original");
