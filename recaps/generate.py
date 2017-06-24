fc9ff110e0dfa00616b215d2ffa75a9b92eef542
Tom Gibson <beyamor@gmail.com>
2017-06-24 09:33:25 -0700

    Updated the contestoid link

--- recaps/generate.py
+++ recaps/generate.py
@@ -1,7 +1,7 @@
 CATEGORIES = {
 		"Topsaucetoid": "http://farm3.static.flickr.com/2777/4068256421_cf820647b2_o.jpg",
 		"Wordtoid": "http://farm3.static.flickr.com/2800/4069011354_b7e15681cd_o.jpg",
-		"Contestoid": "http://farm3.static.flickr.com/2708/4068256087_e691dde06e_o.jpg",
+                "Contestoid": "https://www.destructoid.com/ul/user/2/25438-444441-ContestoidBannerjpg-noscale.jpg",
 		"Communitoid": "http://farm3.static.flickr.com/2615/4069011212_6af1534a1c_o.jpg",
 		"Gametoid": "http://farm4.static.flickr.com/3509/4068256301_b326be1ecd_o.jpg",
 		"Culturoid": "http://farm3.static.flickr.com/2759/4068256383_1c3511b1fe_o.jpg",
